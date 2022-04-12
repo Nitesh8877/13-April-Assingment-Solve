fed71e24402ceb30b9c01f22271b7f970993bc9f
Nitesh Kumar <82534120+Nitesh8877@users.noreply.github.com>
2022-04-13 03:05:37 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -9,11 +9,17 @@ const employees = {
             name: "Nitesh",
             occupation: "Front-end Engineer"
         },
+        education:{
+            School:'jai swal high school',
+            board:'BSEB',
+            Year:2017
+        }
     
 };
 
 const {1:{id,name,occupation}}=employees;
 const {1:{...arr},2:{...arr2}}=employees;
+
 
 console.log(id);
 console.log(name);
@@ -21,3 +27,8 @@ console.log(occupation);
 console.log("-------------------------------")
 console.log(arr);
 console.log(arr2);
+
+const {School,board,Year}=3;
+
+console.log("This is my education School Name"+School+" and board Nmae: "+board+" year "+Year);
+
